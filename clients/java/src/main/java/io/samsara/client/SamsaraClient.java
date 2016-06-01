a681e9d52732aaf6b7913d8b6ffff2f5d14190a0
Arnaud Bos <arnaud.tlse@gmail.com>
2016-06-01 14:00:49 +0200

    Init java client

---
+++ clients/java/src/main/java/io/samsara/client/SamsaraClient.java
@@ -0,0 +1,7 @@
+package io.samsara.client;
+
+/**
+ * Created by arnaud on 01/06/16.
+ */
+public class SamsaraClient {
+}
---
+++ clients/java/src/test/java/io/samsara/client/SamsaraClientTest.java
@@ -0,0 +1,10 @@
+package io.samsara.client;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by arnaud on 01/06/16.
+ */
+public class SamsaraClientTest {
+
+}
